1a8a315fd5aae7e8a79c4986a7aaaaaed7c722ca
Mathew Davies <thepixeldeveloper@googlemail.com>
2013-02-02 18:16:51 +0000

    Write a test to confirm out index output.

---
+++ tests/Sitemap/Writers/IndexTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Sitemap\Writers;
+
+use Sitemap\Sitemap;
+use Sitemap\Index;
+
+class IndexTest extends \PHPUnit_Framework_TestCase
+{
+    public function testIndexXMLWriter()
+    {
+        $sitemap1 = new Sitemap;
+        $sitemap1->setLocation('http://www.example.com/sitemap1.xml.gz');
+        $sitemap1->setLastMod('2004-10-01T18:23:17+00:0');
+
+        $sitemap2 = new Sitemap;
+        $sitemap2->setLocation('http://www.example.com/sitemap2.xml.gz');
+        $sitemap2->setLastMod('2005-01-01');
+
+        $index = new Index;
+        $index->addSitemap($sitemap1);
+        $index->addSitemap($sitemap2);
+
+        $writer = new \Sitemap\Writers\XML\Index($index);
+
+        $this->assertXmlStringEqualsXmlFile(__DIR__.'/../../controls/index.xml', (string) $writer->output());
+    }
+}
