d395934684c23f2df1f2ab2e3fcf5413fe2943c9
Mathew Davies <thepixeldeveloper@googlemail.com>
2013-02-02 18:25:58 +0000

    Create an XML renderer for basic sitemaps.

---
+++ src/Sitemap/Writers/XML/Sitemap/Basic.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Sitemap\Writers\XML\Sitemap;
+
+class Basic extends \Sitemap\Writers\XML\Sitemap
+{
+    private $sitemap;
+
+    public function __construct(\Sitemap\Sitemap\Basic $sitemap)
+    {
+        $this->sitemap = $sitemap;
+    }
+
+    public function output()
+    {
+        $writer = $this->writer();
+        $writer->writeRaw(parent::output());
+        $writer->writeElement('changefreq', $this->sitemap->getChangeFreq());
+        $writer->writeElement('priority', $this->sitemap->getPriority());
+        return $writer->flush();
+    }
+}
