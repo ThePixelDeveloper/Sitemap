03dfd2ac1301a06a7b2ffc11706b52596ee6d331
Mathew Davies <thepixeldeveloper@googlemail.com>
2014-08-25 13:23:35 +0100

    Pass in the location to the SitemapEntry test.

--- tests/Sitemap/URLSetTest.php
+++ tests/Sitemap/URLSetTest.php
@@ -9,15 +9,13 @@ class URLSetTest extends \PHPUnit_Framework_TestCase
 {
     public function testBasicXMLWriter()
     {
-        $basic1 = new SitemapEntry;
+        $basic1 = new SitemapEntry('http://www.example.com/');
         $basic1->setPriority(0.8);
         $basic1->setChangeFreq('monthly');
         $basic1->setLastMod('2005-01-01');
-        $basic1->setLocation('http://www.example.com/');
 
-        $basic2 = new SitemapEntry;
+        $basic2 = new SitemapEntry('http://www.example.com/catalog?item=12&desc=vacation_hawaii');
         $basic2->setChangeFreq('weekly');
-        $basic2->setLocation('http://www.example.com/catalog?item=12&desc=vacation_hawaii');
 
         $urlsetCollection = new Collection;
         $urlsetCollection->addSitemap($basic1);
