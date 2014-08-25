5b7ac5b8fda8d30617a5a4e7869ac56201417a32
Mathew Davies <thepixeldeveloper@googlemail.com>
2014-08-25 13:23:03 +0100

    Pass in the location to the SitemapEntry test.

--- tests/Sitemap/SitemapIndexTest.php
+++ tests/Sitemap/SitemapIndexTest.php
@@ -9,17 +9,14 @@ class SitemapIndexTest extends \PHPUnit_Framework_TestCase
 {
     public function testDuplicateEntries()
     {
-        $sitemap1 = new SitemapEntry;
-        $sitemap1->setLocation('http://www.example.com/sitemap1.xml.gz');
+        $sitemap1 = new SitemapEntry('http://www.example.com/sitemap1.xml.gz');
         $sitemap1->setLastMod('2004-10-01T18:23:17+00:00');
 
         // Duplicate entries start.
-        $sitemap2 = new SitemapEntry;
-        $sitemap2->setLocation('http://www.example.com/sitemap2.xml.gz');
+        $sitemap2 = new SitemapEntry('http://www.example.com/sitemap2.xml.gz');
         $sitemap2->setLastMod('2005-01-01');
 
-        $sitemap3 = new SitemapEntry;
-        $sitemap3->setLocation('http://www.example.com/sitemap2.xml.gz');
+        $sitemap3 = new SitemapEntry('http://www.example.com/sitemap2.xml.gz');
         $sitemap3->setLastMod('2005-01-01');
         // Duplicate entries end.
 
