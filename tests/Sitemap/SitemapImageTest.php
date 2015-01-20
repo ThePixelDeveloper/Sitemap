4da839a349c16635b8caee348d7d501c3bead5de
Adam Mahmood <adammahmood2@gmail.com>
2015-01-20 19:07:00 +0000

    updated unit tests and README

--- tests/Sitemap/SitemapImageTest.php
+++ tests/Sitemap/SitemapImageTest.php
@@ -10,15 +10,17 @@ class SitemapImageTest extends \PHPUnit_Framework_TestCase
 {
     public function testBasicImagesXMLWriter()
     {
-        $basic1 = new SitemapImageEntry('http://www.example.com/1');
+        // Image 1
         $image1 = new ImageEntry('https://s3.amazonaws.com/path/to/image');
 
+        // Image 2 with multiple attributes
         $image2 = new ImageEntry('https://s3.amazonaws.com/path/to/image2');
         $image2->setCaption('Test Caption');
         $image2->setGeoLocation('Limerick, Ireland');
         $image2->setTitle('Test Title');
         $image2->setLicense('http://www.license.com');
 
+        $basic1 = new SitemapImageEntry('http://www.example.com/1');
         $basic1->addImages($image1);
         $basic1->addImages($image2);
 
@@ -26,14 +28,14 @@ class SitemapImageTest extends \PHPUnit_Framework_TestCase
         $basic2->addImages($image1);
         $basic2->addImages($image2);
 
-        $urlsetCollection = new Collection;
-        $urlsetCollection->addSitemap($basic1);
-        $urlsetCollection->addSitemap($basic2);
-        $urlsetCollection->setFormatter(new SitemapImage);
+        $collection = new Collection;
+        $collection->addSitemap($basic1);
+        $collection->addSitemap($basic2);
+        $collection->setFormatter(new SitemapImage);
 
         $this->assertXmlStringEqualsXmlFile(
             __DIR__.'/../controls/image.xml',
-            (string) $urlsetCollection->output()
+            (string) $collection->output()
         );
     }
 
