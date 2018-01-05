a856ea608ba21fc10fe356869477d2a86e791f91
Mathew Davies <ThePixelDeveloper@users.noreply.github.com>
2018-01-05 20:34:07 +0000

    Update CompleteTest.php

--- tests/CompleteTest.php
+++ tests/CompleteTest.php
@@ -91,7 +91,7 @@ XML;
 
         $expected = <<<XML
 <?xml version="1.0" encoding="UTF-8"?>
-<sitemapindex xmlns:xsi="https://www.w3.org/2001/XMLSchema-instance"
+<sitemapindex xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
               xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/siteindex.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <sitemap>
         <loc>http://example.com</loc>
