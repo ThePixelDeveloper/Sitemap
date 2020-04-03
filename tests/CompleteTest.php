ee4cbd1406f1250534860b8c9b74020fee0c3650
Jamie Balish <redjam13@users.noreply.github.com>
2020-04-03 11:27:23 +0100

    fix: use correct schemaLocation for urlset (#69)

--- tests/CompleteTest.php
+++ tests/CompleteTest.php
@@ -39,7 +39,7 @@ class CompleteTest extends TestCase
 
         $expected = <<<XML
 <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/siteindex.xsd"
+        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
         xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
         xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:xhtml="http://www.w3.org/1999/xhtml"
         xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0"
